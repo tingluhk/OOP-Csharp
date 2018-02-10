3d5dfe4c6a5c38ee571e53a90c55776c4349d5b0
tingluhk <tingluhk@gmail.com>
2018-02-10 17:40:01 +0800

    added Person Class and added Method01 in Program.cs

---
+++ Projects/Console01/Person.cs
@@ -0,0 +1,4 @@
+class Person{
+    public int Age { get; set; }
+    public string Name { get; set; }
+}
--- Projects/Console01/Program.cs
+++ Projects/Console01/Program.cs
@@ -6,7 +6,14 @@ namespace Console01
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World! whats going on");
+            Method01(22,"john");
+        }
+
+        public static void Method01(int Age, string Name){
+            Person person01 = new Person();
+            person01.Name = Name;
+            person01.Age = Age;
+            System.Console.WriteLine($"my name is {person01.Name}, my age is {person01.Age}");
         }
     }
 }
